90d7961d001fea78456e77f6a3ff6c5924640c5f
Naufal Zaky <naufalzaky754@gmail.com>
2022-12-28 23:56:35 +0700

    pust test automation
    pust test automation

--- test/specs/mobile.automation.js
+++ test/specs/mobile.automation.js
@@ -1,8 +1,21 @@
-describe('Open application', () => {
-  it('Success open the apps', async () => {
-    driver.setImplicitTimeout(5000);
-    const selector = 'new UiSelector().text("Welcome...").className("android.widget.TextView")';
-    await expect(await $(`android=${selector}`)).toHaveTextContaining("Welcome");
-    driver.closeApp();
+describe("Open Diet Meal App", () => {
+  it("Flow Diet Meal App", async () => {
+    const inputName = await $("id=com.fghilmany.dietmealapp:id/et_name");
+    const inputWeight = await $("id=com.fghilmany.dietmealapp:id/et_weight");
+    const inputHeight = await $("id=com.fghilmany.dietmealapp:id/et_height");
+    const chooseGender = await $("id=com.fghilmany.dietmealapp:id/rb_male");
+    const btnNext = await $("id=com.fghilmany.dietmealapp:id/bt_next");
+    const actRest = await $("id=com.fghilmany.dietmealapp:id/rb_rest");
+    const btnFinish = await $("id=com.fghilmany.dietmealapp:id/bt_finish");
+    const homepage = await $("id=com.fghilmany.dietmealapp:id/tv_header_desc");
+
+    await inputName.setValue("Naufal");
+    await inputWeight.setValue("70");
+    await inputHeight.setValue("171");
+    await chooseGender.click();
+    await btnNext.click();
+    await actRest.click();
+    await btnFinish.click();
+    await expect(homepage).toHaveAttribute("text", "Ready to some calories today?");
   });
 });
